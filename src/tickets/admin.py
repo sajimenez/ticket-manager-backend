88980dcf201b891ab79893b73cebe8a559754e96
Sebastian Jimenez <sajimenez5@gmail.com>
2020-05-23 00:21:10 -0400

    Customized Admin Site

--- src/tickets/admin.py
+++ src/tickets/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 
 from .models import Ticket
 
-admin.site.register(Ticket)
+
+class TicketAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'status', 'created', 'creator', )
+    list_filter = ('status', )
+    search_fields = ('id', 'title', )
+    ordering = ('-created', )
+
+
+admin.site.register(Ticket, TicketAdmin)
